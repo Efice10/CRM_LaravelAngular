be2ce7764a535572cb45b30886fc8923d8d28ad9
Shahrul <shahrulnizam.5121@gmail.com>
2023-07-31 09:11:28 +0800

    commit

--- CRMProject-main/frontend/src/app/app.module.ts
+++ CRMProject-main/frontend/src/app/app.module.ts
@@ -5,12 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableAdminComponent } from './components/admin/table-admin/table-admin.component';
+import { CreateAdminComponent } from './components/admin/create-admin/create-admin.component';
+import { ShowAdminComponent } from './components/admin/show-admin/show-admin.component';
+import { EditAdminComponent } from './components/admin/edit-admin/edit-admin.component';
+import { CreateUsersComponent } from './components/users/create-users/create-users.component';
+import { CreateCompanyComponent } from './components/company/create-company/create-company.component';
+import { EditCompanyComponent } from './components/company/edit-company/edit-company.component';
+import { DeleteCompanyComponent } from './components/company/delete-company/delete-company.component';
+import { CreateProjectComponent } from './components/project/create-project/create-project.component';
+import { EditProjectComponent } from './components/project/edit-project/edit-project.component';
+import { DeleteProjectComponent } from './components/project/delete-project/delete-project.component';
+import { DeleteUsersComponent } from './components/users/delete-users/delete-users.component';
+import { EditUsersComponent } from './components/users/edit-users/edit-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    TableAdminComponent,
+    CreateAdminComponent,
+    ShowAdminComponent,
+    EditAdminComponent,
+    CreateUsersComponent,
+    CreateCompanyComponent,
+    EditCompanyComponent,
+    DeleteCompanyComponent,
+    CreateProjectComponent,
+    EditProjectComponent,
+    DeleteProjectComponent,
+    DeleteUsersComponent,
+    EditUsersComponent,
   ],
   imports: [
     BrowserModule,
---
+++ CRMProject-main/frontend/src/app/components/admin/table-admin/table-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-admin',
+  templateUrl: './table-admin.component.html',
+  styleUrls: ['./table-admin.component.scss']
+})
+export class TableAdminComponent {
+
+}
